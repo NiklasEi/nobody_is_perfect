e7091fa3aa7ecc6731584d983466352fa76bc3f7
Niklas Eicker <git@nikl.me>
2021-04-02 14:39:20 +0200

    Resolve all ToDos and fix compilation warnings

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
 // disable console opening on windows
 #![windows_subsystem = "windows"]
 
-use bevy::prelude::{App, Msaa, WindowDescriptor, ClearColor, Color};
+use bevy::prelude::{App, ClearColor, Color, Msaa, WindowDescriptor};
 use bevy::DefaultPlugins;
 use game_plugin::GamePlugin;
 
 fn main() {
     let mut app = App::build();
-    app
-        // .insert_resource(Msaa { samples: 4 })
+    app.insert_resource(Msaa { samples: 4 })
         .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
         .insert_resource(WindowDescriptor {
             width: 800.,
             height: 600.,
-            title: "Bevy game".to_string(), // ToDo
+            title: "Nobody is perfect".to_string(),
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)

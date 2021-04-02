a7664ad9fc9155e264e7e149f041ca00ee936944
Niklas Eicker <git@nikl.me>
2021-04-02 21:55:11 +0200

    Add field of view

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Nobody is perfect | Game for Mini Jam 77
 
-*Theme: Courage*
-*Limitation: Game cannot take place on earth*
+*Theme: Courage | Limitation: Game cannot take place on earth*
 
 You play as a triangle that is scared of others it doesn't know. It feels like all the other forms are perfect (circles), only itself has corners.
 
--- game_plugin/src/player.rs
+++ game_plugin/src/player.rs
@@ -1,24 +1,36 @@
 use bevy::prelude::*;
 use crate::actions::Actions;
 use crate::{GameState, GameWorld};
-use bevy_prototype_lyon::prelude::{FillOptions, GeometryBuilder};
+use bevy_prototype_lyon::prelude::{FillOptions, GeometryBuilder, Geometry, PathBuilder, StrokeOptions};
 use bevy_prototype_lyon::entity::ShapeColors;
 use bevy_prototype_lyon::utils::DrawMode;
 use bevy_prototype_lyon::shapes;
+use std::f32::consts::PI;
 
 pub struct PlayerPlugin;
 
 pub struct Player;
 pub struct PlayerCamera;
+pub struct  FieldOfView;
+
+#[derive(SystemLabel, Clone, Hash, Debug, Eq, PartialEq)]
+enum PlayerSystemLabels {
+    MovePlayer
+}
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.insert_resource(CursorPosition {
             position: Vec2::new(0., 0.)
-        }).add_system_set(SystemSet::on_enter(GameState::Playing).with_system(spawn_player.system()).with_system(spawn_camera.system()))
+        }).add_system_set(SystemSet::on_enter(GameState::Playing)
+            .with_system(spawn_player.system())
+            .with_system(spawn_field_of_view.system())
+            .with_system(spawn_camera.system()))
             .add_system_set(
                 SystemSet::on_update(GameState::Playing)
-                    .with_system(move_player.system()),
+                    .with_system(cursor_grab_system.system())
+                    .with_system(move_player.system().label(PlayerSystemLabels::MovePlayer))
+                    .with_system(move_field_of_view.system().after(PlayerSystemLabels::MovePlayer)),
             )
             .add_system_set(SystemSet::on_exit(GameState::Playing).with_system(remove_player.system()));
 
@@ -35,6 +47,21 @@ fn spawn_camera(mut commands: Commands) {
         .insert(PlayerCamera);
 }
 
+fn cursor_grab_system(
+    mut windows: ResMut<Windows>,
+    button: Res<Input<MouseButton>>,
+    key: Res<Input<KeyCode>>,
+) {
+    let window = windows.get_primary_mut().unwrap();
+
+    if button.just_pressed(MouseButton::Left) {
+        window.set_cursor_lock_mode(true);
+    }
+
+    if key.just_pressed(KeyCode::Escape) {
+        window.set_cursor_lock_mode(false);
+    }
+}
 
 fn spawn_player(
     mut commands: Commands,
@@ -52,9 +79,34 @@ fn spawn_player(
                 outline: Color::ANTIQUE_WHITE
             },
             DrawMode::Fill(FillOptions::default()),
-            Transform::from_translation(Vec3::new(0., 0., 0.)),
+            Transform::from_translation(Vec3::new(0., 0., 1.)),
         ))
         .insert(Player);
+}
+
+pub fn field_of_view() -> impl Geometry {
+    let mut builder = PathBuilder::new();
+    builder.move_to(Vec2::ZERO);
+    builder.line_to(Vec2::new(-35., 150.));
+    builder.arc(Vec2::new(-0., 150.), Vec2::new(35., 35.), -PI, -PI);
+    builder.line_to(Vec2::ZERO);
+    builder.build()
+}
+
+fn spawn_field_of_view(
+    mut commands: Commands,
+) {
+    commands
+        .spawn_bundle(GeometryBuilder::build_as(
+            &field_of_view(),
+            ShapeColors {
+                main: Color::GRAY,
+                outline: Color::ANTIQUE_WHITE
+            },
+            DrawMode::Fill(FillOptions::default()),
+            Transform::from_translation(Vec3::new(0., 0., 2.)),
+        ))
+        .insert(FieldOfView);
 }
 
 fn move_player(
@@ -93,8 +145,20 @@ fn move_player(
     }
 }
 
-fn remove_player(mut commands: Commands, player_query: Query<Entity, With<Player>>) {
+fn move_field_of_view(player_query: Query<&Transform,(With<Player>, Without<FieldOfView>)>, mut field_of_view_query: Query<&mut Transform,( With<FieldOfView>, Without<Player>)>) {
+    for player_transform in player_query.iter() {
+        for mut fov_transform in field_of_view_query.iter_mut() {
+            fov_transform.translation = Vec3::new(player_transform.translation.x, player_transform.translation.y, 0.);
+            fov_transform.rotation = player_transform.rotation;
+        }
+    }
+}
+
+fn remove_player(mut commands: Commands, player_query: Query<Entity, With<Player>>, mut windows: ResMut<Windows>,) {
     for player in player_query.iter() {
         commands.entity(player).despawn();
     }
+
+    let window = windows.get_primary_mut().unwrap();
+    window.set_cursor_lock_mode(false);
 }
